0ca6ab115fc0eda00662163e86ed591faea7f731
“[krishna-droidmax]” <ks7599020645@gmail.com>
2025-08-14 11:14:36 +0530

    your commit message

---
+++ Area_Of_A_Circle.java
@@ -0,0 +1,7 @@
+public class Area_Of_A_Circle {
+    public static void main(String[] args) {
+        double radius = 5.0;
+        double area = Math.PI * radius * radius;
+        System.out.println("The area of a circle with radius " + radius + " is: " + area);
+    }
+}
