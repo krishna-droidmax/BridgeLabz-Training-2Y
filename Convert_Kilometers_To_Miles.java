eecf4771ff57cc0c75b2a93160056a9211dcc7dc
“[krishna-droidmax]” <ks7599020645@gmail.com>
2025-08-14 11:38:06 +0530

    your commit message

---
+++ Convert_Kilometers_To_Miles.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Convert_Kilometers_To_Miles {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter distance in kilometers: ");
+        double kilometers = scanner.nextDouble();
+
+        double miles = kilometers * 0.621371;
+        System.out.println(kilometers + " kilometers is equal to " + miles + " miles.");
+
+        scanner.close();
+    }
+}
