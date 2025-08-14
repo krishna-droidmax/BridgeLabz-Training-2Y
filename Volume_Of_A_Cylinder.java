40760bd417deb950e8f8b8d4a8cbfd61fe08655f
“[krishna-droidmax]” <ks7599020645@gmail.com>
2025-08-14 11:15:55 +0530

    volume of a cylinder

---
+++ Volume_Of_A_Cylinder.java
@@ -0,0 +1,8 @@
+public class Volume_Of_A_Cylinder {
+    public static void main(String[] args) {
+        double radius = 5.0;
+        double height = 10.0;
+        double volume = Math.PI * radius * radius * height;
+        System.out.println("The volume of a cylinder with radius " + radius + " and height " + height + " is: " + volume);
+    }
+}
